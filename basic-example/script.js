9ae093da0831a11924b6d38affdcfbf9bf36d8f4
FaseehFS <faseeh1080@gmail.com>
2026-03-06 17:55:29 +0530

    Update color naming convensions

--- basic-example/script.js
+++
@@ -1,7 +0,0 @@
-const donateBtn = document.getElementById("donate-btn");
-
-donateBtn.addEventListener("click", function () {
-  /* "this" won't work with arrow functions */
-  this.innerText = "Thanks";
-  this.disabled = true;
-});
